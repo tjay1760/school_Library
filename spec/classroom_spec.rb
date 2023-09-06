e5479aba2339f448985b6791c957a5f9d73a8e04
John Murianki <mcjthiongo@gmail.com>
2023-09-07 02:42:31 +0300

    Add decorator test

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../classroom.rb'
+describe Classroom do
+    describe "#Classroom" do
+        it "creates a new Classroom" do
+            classroom = Classroom.new("Javascript")
+            expect(classroom.label).to eq('Javascript')
+        end
+        it "check valid class" do
+            expect { Classroom.new }.to raise_exception(ArgumentError)
+                end
+    end
+
+end
